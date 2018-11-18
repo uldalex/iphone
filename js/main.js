3e04e7f9a350206adc79eee3a6680a7b711de987
uldalex <uldalex@gmail.com>
2018-11-18 17:57:09 +0300

    1

---
+++ js/main.js
@@ -0,0 +1,29 @@
+// Импортируем jQuery
+//= ../bower_components/jquery/dist/jquery.js
+ 
+// Импортируем Popper
+//= ../bower_components/popper.js/dist/umd/popper.js
+ 
+// Импортируем необходимые js-файлы Bootstrap 4
+//= ../bower_components/bootstrap/js/dist/util.js
+//= ../bower_components/bootstrap/js/dist/alert.js
+//= ../bower_components/bootstrap/js/dist/button.js
+//= ../bower_components/bootstrap/js/dist/carousel.js
+//= ../bower_components/bootstrap/js/dist/collapse.js
+//= ../bower_components/bootstrap/js/dist/dropdown.js
+//= ../bower_components/bootstrap/js/dist/modal.js
+//= ../bower_components/bootstrap/js/dist/scrollspy.js
+//= ../bower_components/bootstrap/js/dist/tab.js
+//= ../bower_components/bootstrap/js/dist/tooltip.js
+//= ../bower_components/bootstrap/js/dist/popover.js
+ 
+// Импортируем другие js-файлы
+//= lightbox.js
+//= scripts.min.js
+//= /Infinity/infinite-scroll.pkgd.min.js
+//= /front/modals/loginModal.js
+//= jquery.mousewheel.js
+//= js/jquery.bxslider.min.js
+//= jquery.scrollbar.min.js
+//= masonry.pkgd.min.js
+//= new.script.js
